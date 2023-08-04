e30a91d0a2e0e5b55b3bccd5809275f0e7e3e551
Abdullah Akhlaq <abdullah.akhlaq@ceative.co.uk>
2023-08-04 15:34:54 +0500

    #comment use enlisnt

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import {useState} from 'react'
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+const [first, setfirst] = useState('')
+  return (
+    <main className={styles.main}>
+    hfkrfjkjvbhk j    fdkjghcjkv hsdkjvhdsjkv vjkhvdkj hkjfshf
+    </main>
+  )
+}
